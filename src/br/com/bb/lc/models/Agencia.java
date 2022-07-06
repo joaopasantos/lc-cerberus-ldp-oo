d99e3515398b5a2a771d50631d1ad532f2bc7e2d
F4957995 Joao Pedro Albuquerque dos Santos <joaopasantos@bb.com.br>
2022-07-06 14:13:26 -0300

    Implementa métodos básicos de Conta e classe Agencia

---
+++ src/br/com/bb/lc/models/Agencia.java
@@ -0,0 +1,6 @@
+package br.com.bb.lc.models;
+
+public class Agencia {
+    private String numero;
+    private Endereco endereco;
+}
--- src/br/com/bb/lc/models/Conta.java
+++ src/br/com/bb/lc/models/Conta.java
@@ -2,9 +2,67 @@ package br.com.bb.lc.models;
 
 import java.math.BigDecimal;
 
+//sacar, depositar, transferência, investir e consultar saldo (poupança e corrente).
+//sendo que para PJ existe a cobrança de uma taxa de 0.5% para cada saque ou transferência.
 public abstract class Conta {
     private String numero;
-    // private Agencia agencia;
+    private Agencia agencia;
     private Pessoa titular;
     private BigDecimal saldo;
+
+    public BigDecimal sacar(BigDecimal valor) {
+        if (valor.signum() == -1)
+            throw new RuntimeException("Valor de saque inválido");
+
+        // Para PJ, existe a cobrança de uma taxa de 0.5% para cada saque ou
+        // transferência.
+        if (this.titular instanceof PessoaJuridica)
+            valor.multiply(new BigDecimal("1.05"));
+
+        if (this.saldo.compareTo(valor) == -1)
+            throw new RuntimeException("Saldo insuficiente para operação");
+
+        this.saldo = saldo.subtract(valor);
+        return valor;
+    }
+
+    public BigDecimal depositar(BigDecimal valor) {
+        if (valor.signum() == -1)
+            throw new RuntimeException("Valor de depósito inválido");
+        this.saldo = saldo.add(valor);
+        return valor;
+    }
+
+    public BigDecimal transferir(Conta destino, BigDecimal valor) {
+        if (valor.signum() == -1)
+            throw new RuntimeException("Valor de transferência inválido");
+        BigDecimal valorRetirado = this.sacar(valor);
+        destino.depositar(valor);
+        return valorRetirado;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Agencia getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(Agencia agencia) {
+        this.agencia = agencia;
+    }
+
+    public Pessoa getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Pessoa titular) {
+        this.titular = titular;
+    }
+
 }

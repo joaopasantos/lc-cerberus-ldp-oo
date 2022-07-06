effef69b82ba44565a0f666bbcbef8eb2ddfb441
F4957995 Joao Pedro Albuquerque dos Santos <joaopasantos@bb.com.br>
2022-07-06 14:14:30 -0300

    Gera getters e setters das respectivas classes

--- src/br/com/bb/lc/models/Agencia.java
+++ src/br/com/bb/lc/models/Agencia.java
@@ -3,4 +3,20 @@ package br.com.bb.lc.models;
 public class Agencia {
     private String numero;
     private Endereco endereco;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
--- src/br/com/bb/lc/models/Endereco.java
+++ src/br/com/bb/lc/models/Endereco.java
@@ -8,4 +8,71 @@ public class Endereco {
     private String logradouro;
     private String numeroResidencia;
     private String complemento;
+
+    public Endereco(String cEP, String estado, String cidade, String bairro, String logradouro, String numeroResidencia,
+            String complemento) {
+        CEP = cEP;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.logradouro = logradouro;
+        this.numeroResidencia = numeroResidencia;
+        this.complemento = complemento;
+    }
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String cEP) {
+        CEP = cEP;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumeroResidencia() {
+        return numeroResidencia;
+    }
+
+    public void setNumeroResidencia(String numeroResidencia) {
+        this.numeroResidencia = numeroResidencia;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 }
